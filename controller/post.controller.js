ad46788980f72dc4f08b9295ea87e593302075cc
lanneq <0xdf00ff@gmail.com>
2023-01-29 18:08:08 +0500

    getPost controller created

--- controller/post.controller.js
+++ controller/post.controller.js
@@ -3,14 +3,13 @@ const db = require('../db')
 class PostController {
   async cretePost(req, res) {
     const { title, content } = req.body;
-    const newPost = await db.query('INSERT INTO post (title, content) values ($1, $2) RETURNING *', [title, content])
-    res.json(newPost.rows[0])
+    const newPost = await db.query('INSERT INTO post (title, content) values ($1, $2) RETURNING *', [title, content]);
+    res.json(newPost.rows[0]);
   }
 
   async getPosts(req, res) {
     const posts = await db.query('SELECT * FROM post');
-    // res.json(posts.rows.sort((a, b) => a.id - b.id))
-    res.json(posts.rows)
+    res.json(posts.rows);
   }
 }
 
